8b0780556b8102d12ab2ff783f38101ec3a2ffa5
Thibaut <thibaut@adapt-Louis.lal.in2p3.fr>
2018-11-27 12:31:58 +0100

    first commit

---
+++ utils.py
@@ -0,0 +1,172 @@
+import numpy as np
+import pylab as plt
+import camb
+import fisher_dict
+import sys
+import V3calc as V3
+import os
+
+def get_noise(p,exp):
+    l=np.arange(2,p['lmax'])
+    if p['use_external_file_%s'%exp] is None:
+        sigma=np.array(p['noise_%s'%exp])
+        beam_FWHM=np.array(p['beam_%s'%exp])
+        freq=np.array(p['freq_%s'%exp])
+        sigma_rad=sigma*np.pi/(60*180)
+        beam_FWHM_rad=beam_FWHM*np.pi/(60*180)
+        beam=beam_FWHM_rad/np.sqrt(8*np.log(2))
+        DNl_array={}
+        DNl_all=0
+        count=0
+        for f in freq:
+            DNl_array[f]=sigma_rad[count]**2*np.exp(l*(l+1)*beam[count]**2)*l*(l+1)/(2*np.pi)
+            DNl_all+=1/DNl_array[f]
+            count+=1
+        DNl_array['all']=1/DNl_all
+    else:
+        print 'use external file'
+        freq_all=np.array(p['freq_all_%s'%exp])
+        ell,N_ell_T_LA,N_ell_P_LA,Map_white_noise_levels=V3.so_V3_LA_noise(2,p['fsky'],p['lmax'],delta_ell=1,beam_corrected=True)
+        freq=np.array(p['freq_%s'%exp])
+        DNl_array={}
+        DNl_all=0
+        count=0
+        for f1 in freq_all:
+            for f2 in freq:
+                if f1==f2:
+                    DNl_array[f1]=N_ell_T_LA[count]*l*(l+1)/(2*np.pi)
+                    DNl_all+=1/DNl_array[f1]
+            count+=1
+        DNl_array['all']=1/DNl_all
+    return(freq,DNl_array)
+
+def get_theory_cls(p):
+    pars = camb.CAMBparams()
+    camb.lensing.ALens.value=p['Alens']
+    if p['H0'] is not None:
+        pars.set_cosmology(H0=p['H0'], ombh2=p['ombh2'], omch2=p['omch2'], mnu=p['mnu'], omk=p['omk'], tau=p['tau'])
+    else:
+        pars.set_cosmology(H0=None,cosmomc_theta=p['theta100']/100., ombh2=p['ombh2'], omch2=p['omch2'], mnu=p['mnu'], omk=p['omk'], tau=p['tau'])
+
+    pars.InitPower.set_params(As=np.exp(p['ln_ten_to_ten_As'])/1e10,ns=p['ns'], r=p['r'])
+    print 'theta',p['theta100']/100.
+    print 'ombh2',p['ombh2']
+    print 'omch2',p['omch2']
+    print 'As',np.exp(p['ln_ten_to_ten_As'])/1e10
+    print 'ns',p['ns']
+
+    pars.set_for_lmax(p['lmax'], lens_potential_accuracy=0)
+    results = camb.get_results(pars)
+    powers =results.get_cmb_power_spectra(pars, CMB_unit='muK')
+    totCL=powers['total']
+    return(totCL)
+
+def bin_spectrum(p,l,dl):
+    lmin=p['lmin']
+    lmax=p['lmax']
+    delta_l=p['delta_l']
+    Nbin=np.int(lmax/delta_l)
+    db=np.zeros(Nbin)
+    lb=np.zeros(Nbin)
+    for i in range(Nbin):
+        id=np.where( (l> i*delta_l) & (l< (i+1)*delta_l))
+        db[i]=np.mean(dl[id])
+        lb[i]=np.mean(l[id])
+    id=np.where(lb>lmin)
+    lb,db=lb[id],db[id]
+    return(lb,db)
+
+def bin_variance(p,l,vl):
+    lmin=p['lmin']
+    lmax=p['lmax']
+    delta_l=p['delta_l']
+    Nbin=np.int(lmax/delta_l)
+    vb=np.zeros(Nbin)
+    lb=np.zeros(Nbin)
+    for i in range(Nbin):
+        id=np.where( (l> i*delta_l) & (l< (i+1)*delta_l))
+        vb[i]=np.sum(1/vl[id])
+        lb[i]=np.mean(l[id])
+    vb=1/vb
+    
+    id=np.where(lb>lmin)
+    lb,vb=lb[id],vb[id]
+    
+    return(lb,vb)
+
+def writeSpectrum(l,Dl,fileName):
+    
+    g = open(fileName,mode="w")
+    for k in xrange(len(l)):
+        g.write("%f %e\n"%(l[k],Dl[k]))
+    g.close()
+
+def variance(p,l,Dl,DNl_all):
+    var= 2./((2*l+1)*p['fsky'])*(Dl+DNl_all)**2
+    return var
+
+def generate_residu_from_variance(var):
+    res=np.sqrt(var)*np.random.randn(len(var))
+    return(res)
+
+def generate_1d_grid(p,param,min,max,num):
+    
+    dir='grid_1d_%s'%param
+    try:
+        os.mkdir(dir)
+    except:
+        pass
+    totCL=get_theory_cls(p)
+    ls=np.arange(2,p['lmax'])
+    Dl_fid=totCL[:,0]
+    Dl_fid=Dl_fid[2:len(ls)+2]
+
+    fid= p[param]
+    values=fid+np.linspace(min,max,num)
+    np.savetxt(dir+'/grid_values.dat',values)
+
+    count=0
+    for v in values:
+        print param,v
+        p[param]= v
+        totCL=get_theory_cls(p)
+        Dl=totCL[:,0][2:len(ls)+2]
+        Diff=Dl-Dl_fid
+        lb,Diff_b= bin_spectrum(p,ls,Diff)
+        writeSpectrum(lb,Diff_b,dir+'/diff_spectrum_%03d.dat'%count)
+        count+=1
+
+def delta2(a,b):
+    if a==b:
+        return 1
+    else:
+        return 0
+
+def delta3(a,b,c):
+    if (a==b) & (b==c):
+        return 1
+    else:
+        return 0
+
+def delta4(a,b,c,d):
+    if (a==b) & (b==c) & (c==d):
+        return 1
+    else:
+        return 0
+
+def f(a,b,c,d,ns):
+    result= 1.*ns[a]*(ns[c]*ns[d]*delta2(a,b)-ns[c]*delta3(a,b,d)-ns[d]*delta3(a,b,c)+delta4(a,b,c,d))
+    result/=(ns[c]*ns[d]*(ns[a]-delta2(a,c))*(ns[b]-delta2(b,d)))
+    return result
+
+def g(a,b,c,d,ns):
+    result= 1.*ns[a]*(ns[c]*delta2(a,b)*delta2(c,d)-delta4(a,b,c,d))
+    result/=(ns[a]*ns[b]*(ns[c]-delta2(a,c))*(ns[d]-delta2(b,d)))
+    return result
+
+def cov(a,b,c,d,ns,ls,Dl,DNl,p):
+    fac= 1./((2*ls+1)*p['fsky'])
+    
+    C=2*Dl**2+Dl*((f(a,c,b,d,ns)+f(a,d,b,c,ns))*DNl[a]+(f(b,d,a,c,ns)+f(b,c,a,d,ns))*DNl[b])+DNl[a]*DNl[b]*(g(a,c,b,d,ns)+g(a,d,b,c,ns))
+    return fac*C
+
